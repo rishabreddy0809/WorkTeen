58a52872ff519c489f95115f6cf266db88ac4c63
Rishab Reddy Paili <rishabreddy0809@gmail.com>
2026-06-24 15:42:20 -0400

    Initial Commit

---
+++ WorkTeen/WorkTeenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkTeenApp.swift
+//  WorkTeen
+//
+//  Created by Rishab Reddy on 6/24/26.
+//
+
+import SwiftUI
+
+@main
+struct WorkTeenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
